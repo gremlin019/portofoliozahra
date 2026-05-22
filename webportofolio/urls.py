20c5fdfdff7285850b04e1b6df696ce056484379
Zahra Nailah Rianto <zahranailahrianto@MacBook-Air-jahraaa.local>
2026-05-22 11:15:54 +0700

    Menambahkan folder docs untuk github pages

--- webportofolio/urls.py
+++ webportofolio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django_distill import distill_path
 from . import views
  
 urlpatterns = [
-    path('',           views.home,       name='home'),
-    path('about/',     views.about,      name='about'),
-    path('skills/',    views.skills,     name='skills'),
-    path('projects/',  views.projects,   name='projects'),
-    path('contact/',   views.contact,    name='contact'),
+    distill_path('',           views.home,       name='home',     distill_file='index.html'),
+    distill_path('about/',     views.about,      name='about',    distill_file='about/index.html'),
+    distill_path('skills/',    views.skills,     name='skills',   distill_file='skills/index.html'),
+    distill_path('projects/',  views.projects,   name='projects', distill_file='projects/index.html'),
+    distill_path('contact/',   views.contact,    name='contact',  distill_file='contact/index.html'),
 ]
