b3bd5acb4bbd3ea995b086ce5e85cf8d22b1956e
Zahra Nailah Rianto <zahranailahrianto@MacBook-Air-jahraaa.local>
2026-05-22 12:11:11 +0700

    Salin aset gambar admin dari folder projects ke docs

--- webportofolio/urls.py
+++ webportofolio/urls.py
@@ -4,8 +4,8 @@ from . import views
  
 urlpatterns = [
     distill_path('',           views.home,       name='home',     distill_file='index.html'),
-    distill_path('about/',     views.about,      name='about',    distill_file='about/index.html'),
-    distill_path('skills/',    views.skills,     name='skills',   distill_file='skills/index.html'),
-    distill_path('projects/',  views.projects,   name='projects', distill_file='projects/index.html'),
-    distill_path('contact/',   views.contact,    name='contact',  distill_file='contact/index.html'),
+    distill_path('about/',     views.about,      name='about',    distill_file='about.html'),
+    distill_path('skills/',    views.skills,     name='skills',   distill_file='skills.html'),
+    distill_path('projects/',  views.projects,   name='projects', distill_file='projects.html'),
+    distill_path('contact/',   views.contact,    name='contact',  distill_file='contact.html'),
 ]
